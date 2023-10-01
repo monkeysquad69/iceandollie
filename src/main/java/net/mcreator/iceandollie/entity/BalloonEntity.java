a0f08774e5944059456c280c971072a07c4f5549
iceandollie <icefighter86@gmail.com>
2023-10-01 14:55:41 +0200

    balloooooooooooon

--- src/main/java/net/mcreator/iceandollie/entity/BalloonEntity.java
+++ src/main/java/net/mcreator/iceandollie/entity/BalloonEntity.java
@@ -111,7 +111,7 @@ public class BalloonEntity extends PathfinderMob {
 	public static AttributeSupplier.Builder createAttributes() {
 		AttributeSupplier.Builder builder = Mob.createMobAttributes();
 		builder = builder.add(Attributes.MOVEMENT_SPEED, 0.3);
-		builder = builder.add(Attributes.MAX_HEALTH, 10);
+		builder = builder.add(Attributes.MAX_HEALTH, 1);
 		builder = builder.add(Attributes.ARMOR, 0);
 		builder = builder.add(Attributes.ATTACK_DAMAGE, 3);
 		builder = builder.add(Attributes.FOLLOW_RANGE, 16);
